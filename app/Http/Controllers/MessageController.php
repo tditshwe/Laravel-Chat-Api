ffe142fe6561a83027a6c958278938d004b822d8
Kgothatso Ditshwene <kgothatsod@bcx.co.za>
2020-02-07 16:27:24 +0200

    Added Firebase messaging

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -9,6 +9,9 @@ use App\Models\Chat;
 use App\Models\User;
 use App\Models\Group;
 
+use Kreait\Firebase;
+use Kreait\Firebase\Messaging\CloudMessage;
+
 class MessageController extends Controller
 {
     public function messages(Request $request, $recipient)
@@ -37,6 +40,18 @@ class MessageController extends Controller
 
     public function sendToUser(Request $request, $recepient)
     {
+        /** Send push notification to device */
+        $deviceToken = 'cwn5vX7TA68QopqE8M9N-C:APA91bEc0xCPp0EYk2wasAKQNypRCEXRPSsgGV0JddAIIvHnV85psvmxePL1FAySQF4VE6FTSmpnrgROWihXbe4sAljpDaG9t9jYtqvkNPhQjSeh82nw8j85Osa8zct0OmRhU-r40Yl5';
+
+        $messaging = (new Firebase\Factory())->createMessaging();
+
+        $message = CloudMessage::fromArray([
+            'token' => $deviceToken,
+            'notification' => ['title' => 'FCM Message', 'body' => 'This is a message from FCM']
+        ]);
+
+        $messaging->send($message);
+
         $chats = Chat::where([['sender', $request->user()->username],['receiver', $recepient]])
                     ->orWhere([['sender', $recepient],['receiver',$request->user()->username]])
                     ->get();
